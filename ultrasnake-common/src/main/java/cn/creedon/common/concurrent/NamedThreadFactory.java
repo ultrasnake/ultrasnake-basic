0ec4e373b55673ceddf09462c3fb4758815c6f2b
WD.c <chuweidong@igetcool.com>
2022-12-16 12:27:15 +0800

    add annotatorn

--- ultrasnake-common/src/main/java/cn/creedon/common/concurrent/NamedThreadFactory.java
+++ ultrasnake-common/src/main/java/cn/creedon/common/concurrent/NamedThreadFactory.java
@@ -3,6 +3,19 @@ package cn.creedon.common.concurrent;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/***
+ * *  _ ___ ___.__   __                  _________              __         ©
+ * * |_   | |  |\  |_/  |_____________   /   _____/ ____ _____  |  | __ ____
+ * *   |  | |  ||  |\   __\_  __ \__  \  \_____  \ /    \\__  \ |  |/ // __ \
+ * *   |  |_|  /|  |_|  |  |  | \// __ \_/        \   |  \/ __ \|    <\  ___/
+ * *   |______/ |____/__|  |__|  (____  /_______  /___|  (____  /__|_ \\___  >
+ * *   UltraSnake - WDC               \/        \/     \/     \/     \/    \/
+ * *
+ * *   功能描述：请在此处加入功能描述
+ * *
+ * *   @DATE    2022/11/18
+ * *   @AUTHOR  WD.C
+ ***/
 public class NamedThreadFactory implements ThreadFactory {
 
     private final ThreadGroup group;
